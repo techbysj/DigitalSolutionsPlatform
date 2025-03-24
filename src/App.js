20d43d96c65a857c8d2433d582936526eea117f9
techbysj <jsabood19@gmail.com>
2025-03-24 12:46:33 +0000

    `Removed unused components`

--- src/App.js
+++ src/App.js
@@ -4,10 +4,7 @@ import Blog from "./pages/Blog";
 import BlogDetails from "./pages/BlogDetails";
 import CaseStudyDetails from "./pages/CaseStudyDetails";
 import Contact from "./pages/Contact";
-import HomeFour from "./pages/HomeFour";
 import HomeOne from "./pages/HomeOne";
-import HomeThree from "./pages/HomeThree";
-import HomeTwo from "./pages/HomeTwo";
 import Service from "./pages/Service";
 import ServiceDetails from "./pages/ServiceDetails";
 import Team from "./pages/Team";
@@ -16,7 +13,6 @@ import AOS from "aos";
 import "aos/dist/aos.css";
 import { useEffect } from "react";
 import ScrollToTop from "react-scroll-to-top";
-import HomeFive from "./pages/HomeFive";
 function App() {
   useEffect(() => {
     AOS.init({
@@ -30,10 +26,6 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route exact path='/' element={<HomeOne />} />
-        <Route exact path='/index-2' element={<HomeTwo />} />
-        <Route exact path='/index-3' element={<HomeThree />} />
-        <Route exact path='/index-4' element={<HomeFour />} />
-        <Route exact path='/index-5' element={<HomeFive />} />
         <Route exact path='/about' element={<About />} />
         <Route exact path='/blog' element={<Blog />} />
         <Route exact path='/blog-details' element={<BlogDetails />} />
