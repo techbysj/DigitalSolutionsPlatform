eddfdeeb255c7596b1f25f9d698a0169b59d2716
techbysj <jsabood19@gmail.com>
2025-03-25 12:55:12 +0000

    `Refactored NavBar.js component with minor changes to HTML attributes and class names`

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -31,17 +31,17 @@ const NavBar = () => {
       {/* search popup start*/}
       <div
         className={searchShow ? "td-search-popup active" : "td-search-popup "}
-        id='td-search-popup'
+        id="td-search-popup"
       >
-        <form action='/' className='search-form'>
-          <div className='form-group'>
+        <form action="/" className="search-form">
+          <div className="form-group">
             <input
-              type='text'
-              className='form-control'
-              placeholder='Search.....'
+              type="text"
+              className="form-control"
+              placeholder="Search....."
             />
           </div>
-          <button type='submit' className='submit-btn'>
+          <button type="submit" className="submit-btn">
             <FaSearch />
           </button>
         </form>
@@ -50,12 +50,12 @@ const NavBar = () => {
       <div
         onClick={searchActive}
         className={searchShow ? "body-overlay active" : "body-overlay"}
-        id='body-overlay'
+        id="body-overlay"
       ></div>
       {/* navbar start */}
-      <nav className='navbar navbar-area navbar-expand-lg'>
-        <div className='container nav-container navbar-bg'>
-          <div className='responsive-mobile-menu'>
+      <nav className="navbar navbar-area navbar-expand-lg">
+        <div className="container nav-container navbar-bg">
+          <div className="responsive-mobile-menu">
             <button
               onClick={menuActive}
               className={
@@ -63,21 +63,21 @@ const NavBar = () => {
                   ? "menu toggle-btn d-block d-lg-none open"
                   : "menu toggle-btn d-block d-lg-none"
               }
-              data-target='#itech_main_menu'
-              aria-expanded='false'
-              aria-label='Toggle navigation'
+              data-target="#itech_main_menu"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
             >
-              <span className='icon-left' />
-              <span className='icon-right' />
+              <span className="icon-left" />
+              <span className="icon-right" />
             </button>
           </div>
-          <div className='logo'>
-            <Link to='/'>
-              <img src='assets/img/logo.png' alt='img' />
+          <div className="logo">
+            <Link to="/">
+              <img src="assets/img/logo.png" alt="img" />
             </Link>
           </div>
-          <div className='nav-right-part nav-right-part-mobile'>
-            <span className='search-bar-btn' onClick={searchActive}>
+          <div className="nav-right-part nav-right-part-mobile">
+            <span className="search-bar-btn" onClick={searchActive}>
               <FaSearch />
             </span>
           </div>
@@ -87,52 +87,43 @@ const NavBar = () => {
                 ? "collapse navbar-collapse sopen"
                 : "collapse navbar-collapse"
             }
-            id='itech_main_menu'
+            id="itech_main_menu"
           >
-            <ul className='navbar-nav menu-open text-lg-end'>
-              <li className='menu-item-has-children'>
-                <a href='#'>Home</a>
+            <ul className="navbar-nav menu-open text-lg-end">
+              <li className="menu-item-has-children">
+                <Link to="/">Home</Link>
               </li>
-              <li className='menu-item-has-children'>
-                <a href='/service'>Services</a>
+              <li className="menu-item-has-children">
+                <Link to="/service">Services</Link>
               </li>
-              <li className='menu-item-has-children'>
-                <a href='#'>Pages</a>
-                <ul className='sub-menu'>
+              <li className="menu-item-has-children">
+                <span
+                  className="nav-link"
+                  onClick={() => console.log("Toggle Pages")}
+                >
+                  Pages
+                </span>
+                <ul className="sub-menu">
                   <li>
-                    <Link to='/about'>About Us</Link>
+                    <Link to="/about">About Us</Link>
                   </li>
                   <li>
-                    <Link to='/team'>Teams</Link>
+                    <Link to="/team">Teams</Link>
                   </li>
-                  {/* <li>
-                    <Link to='/team-details'>Team Details</Link>
-                  </li> */}
-                  {/* <li>
-                    <Link to='/case-study-details'>Case Study Details</Link>
-                  </li> */}
                 </ul>
               </li>
               <li className='menu-item-has-children'>
-                <a href='#'>Blog</a>
-                <ul className='sub-menu'>
-                  {/* <li>
-                    <Link to='/blog'>Blog</Link>
-                  </li> */}
-                  {/* <li>
-                    <Link to='/blog-details'>Blog Details</Link>
-                  </li> */}
-                </ul>
-              </li>
+    <span className="nav-link" onClick={() => console.log("Toggle Blog")}>Blog</span>
+  </li>
               <li>
-                <Link to='/contact'>Contact Us</Link>
+                <Link to="/contact">Contact Us</Link>
               </li>
             </ul>
           </div>
-          <div className='nav-right-part nav-right-part-desktop align-self-center'>
-            <a className='navbar-phone' href='tel:'>
-              <span className='icon'>
-                <img src='assets/img/icon/1.png' alt='img' />
+          <div className="nav-right-part nav-right-part-desktop align-self-center">
+            <a className="navbar-phone" href="tel:">
+              <span className="icon">
+                <img src="assets/img/icon/1.png" alt="img" />
               </span>
               <span>Need help?</span>
               <h5>(+353) 555-0111</h5>
